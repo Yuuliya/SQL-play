69e32b9f23a5bae7aab2a25a2969e0fae0e44d3a
Yuliya Samsonava <yuliya.samsonava@gmail.com>
2021-10-21 10:31:35 -0400

    Update daily-quizz.sql

--- daily-quizz.sql
+++ daily-quizz.sql
@@ -31,3 +31,12 @@ SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2)
 FROM world
 WHERE continent = 'South America'
 
+--5.
+/*
+Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+*/
+SELECT name, ROUND (gdp/population, -3)
+FROM world
+WHERE gdp >= 1000000000000
+
