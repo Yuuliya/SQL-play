4264b28ae9cb32d166e9a427fd226aecc1aaef89
Yuliya Samsonava <yuliya.samsonava@gmail.com>
2021-10-29 17:22:55 -0400

    Update daily-quizz.sql

--- daily-quizz.sql
+++ daily-quizz.sql
@@ -84,13 +84,14 @@ FROM nobel
 WHERE yr = 1980
 AND subject NOT IN ('Chemistry', 'Medicine')
 
---14.
+-26.
 /*
-Find the capital and the name where the capital includes the name of the country.
+Show year, subject, and name of people who won a 'Medicine' prize in an early year 
+(before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004).
 */
-SELECT capital, name
-FROM world
-WHERE capital LIKE concat ('%', name, '%')
+SELECT yr, subject, winner
+FROM nobel
+WHERE (subject = 'Medicine' AND yr < '1910')OR (subject = 'Literature' AND yr >= '2004')
 
 
 -15.
