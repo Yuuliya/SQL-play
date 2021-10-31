5183f043991f7f7ebbc165a5aaad79dd975e4e10
Yuliya Samsonava <yuliya.samsonava@gmail.com>
2021-10-31 14:15:52 -0400

    Update and rename SELECT-within-SELECT to SELECT-within-SELECT.sql

--- SELECT-within-SELECT.sql
+++ SELECT-within-SELECT.sql
@@ -7,3 +7,7 @@ world(name, continent, area, population, gdp)
 /*
 List each country name where the population is larger than that of 'Russia'.
 */
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
