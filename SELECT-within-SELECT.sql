5e871ceb0fcab262a8d936d9628ee35cc04772e8
Yuliya Samsonava <yuliya.samsonava@gmail.com>
2021-11-01 16:22:48 -0400

    Update SELECT-within-SELECT.sql

--- SELECT-within-SELECT.sql
+++ SELECT-within-SELECT.sql
@@ -29,3 +29,11 @@ SELECT name, continent
 FROM world
 WHERE continent IN (SELECT continent FROM world WHERE name = 'Argentina' OR name = 'Australia')
 ORDER BY name
+
+--4.
+/*
+Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+*/
+SELECT name, population
+FROM  world
+WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland')
