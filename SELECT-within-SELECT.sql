6b14a79b305fc354210b99fbb9bd6af664b74f6b
Yuliya Samsonava <yuliya.samsonava@gmail.com>
2021-11-01 16:36:15 -0400

    Update SELECT-within-SELECT.sql

--- SELECT-within-SELECT.sql
+++ SELECT-within-SELECT.sql
@@ -37,3 +37,14 @@ Which country has a population that is more than Canada but less than Poland? Sh
 SELECT name, population
 FROM  world
 WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland')
+
+--5.
+/*
+Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+*/
+SELECT name, CONCAT(ROUND((population*100)/(SELECT population 
+                                 FROM world WHERE name='Germany'), 0), '%')
+FROM world
+WHERE population IN (SELECT population
+                     FROM world
+                     WHERE continent='Europe')
