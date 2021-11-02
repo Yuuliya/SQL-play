2af906a3f963f4bdafc764045d3fa15adff050ff
Yuliya Samsonava <yuliya.samsonava@gmail.com>
2021-11-02 13:19:29 -0400

    Update SELECT-within-SELECT.sql

--- SELECT-within-SELECT.sql
+++ SELECT-within-SELECT.sql
@@ -48,3 +48,11 @@ FROM world
 WHERE population IN (SELECT population
                      FROM world
                      WHERE continent='Europe')
+                     
+ --6.
+ /*
+ Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values).
+*/
+SELECT name 
+  FROM world 
+ WHERE gdp > (SELECT MAX(gdp) FROM world WHERE continent = 'Europe')
