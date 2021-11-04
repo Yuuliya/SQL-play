b2c6eeb4e808909def5b986060d4340c7a2321d1
Yuliya Samsonava <yuliya.samsonava@gmail.com>
2021-11-04 09:23:20 -0400

    Update www.sql-ex.sql

--- www.sql-ex.sql
+++ www.sql-ex.sql
@@ -1,1 +1,16 @@
+/*
+Product(maker, model, type)
+PC(code, model, speed, ram, hd, cd, price)
+Laptop(code, model, speed, ram, hd, screen, price)
+Printer(code, model, color, type, price)
+*/
 
+--1. 
+/*
+Find the model number, speed and hard drive capacity for all the PCs with prices below $500.
+*/
+SELECT model, speed, hd
+FROM PC
+WHERE price < 500
+
+--2.
