bc3b2fcad20d4e6beca4479a033f9f63c659dfc3
Yuliya Samsonava <yuliya.samsonava@gmail.com>
2021-11-05 13:28:34 -0400

    Update www.sql-ex.sql

--- www.sql-ex.sql
+++ www.sql-ex.sql
@@ -28,3 +28,13 @@ Find all records from the Printer table containing data about color printers.
 SELECT *
 FROM Printer
 WHERE color = 'y'
+
+--4
+/*
+For each maker producing laptops with a hard drive capacity of 10 Gb or higher, find the speed of such laptops. Result set: maker, speed.
+*/
+SELECT  DISTINCT maker, speed
+FROM Product INNER JOIN
+Laptop ON Product.model = Laptop.model
+WHERE hd >= 10
+
