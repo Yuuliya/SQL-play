c1dfb1974805ee08b4d79c4fd78b75c267a86119
Yuliya Samsonava <yuliya.samsonava@gmail.com>
2021-11-26 11:49:01 -0500

    Update Khan_Academy_SQL_Challenges.sql

--- Khan_Academy_SQL_Challenges.sql
+++ Khan_Academy_SQL_Challenges.sql
@@ -14,3 +14,5 @@ SELECT title FROM songs;
 /* B. Show the titles of the songs that have an 'epic' mood or a release date after 1990 */
 SELECT title FROM songs WHERE mood = 'epic' OR released > '1990';
 
+/* C. Show the titles of songs that are 'epic', and released after 1990, and less than 4 minutes long */
+SELECT title FROM songs WHERE mood = 'epic' AND released > '1990' AND duration < '240';
