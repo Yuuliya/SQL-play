ee9707cfabad9f26f295c5a5c1318a2c230927a6
Yuliya Samsonava <yuliya.samsonava@gmail.com>
2021-11-26 11:40:39 -0500

    Update Khan_Academy_SQL_Challenges.sql

--- Khan_Academy_SQL_Challenges.sql
+++ Khan_Academy_SQL_Challenges.sql
@@ -1,1 +1,12 @@
+--1.
+/* 
+TABLE songs (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    artist TEXT,
+    mood TEXT,
+    duration INTEGER,
+    released INTEGER);
+ */
+/* A. select all the song titles */
 
